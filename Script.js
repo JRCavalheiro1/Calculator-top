6f423b7502983dd79e3ef89dc14b39423e792b76
Julio Ricardo <juliomilcharek519@gmail.com>
2024-09-24 14:44:01 -0300

    added provisory calculator logic

--- Script.js
+++ Script.js
@@ -8,3 +8,100 @@ isso inclui as funções:
     -DIVISÃO
 
 */
+const numberButtons = document.querySelectorAll(".number-button");
+const operatorButtons = document.querySelectorAll(".operation-button");
+const enterButton = document.querySelector(".enter-button");
+const result = document.querySelector(".result");
+
+
+
+
+function add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+};
+
+function subtract(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+};
+
+function multiply(firstNumber, secondNumber) {
+    return firstNumber * secondNumber;
+};
+
+function divide(firstNumber, secondNumber) {
+    return firstNumber / secondNumber;
+};
+
+
+
+function operate(operator, number1, number2) {
+   
+    switch(operator) {
+        case "+": 
+           return add(number1, number2);
+        break;
+        case "-": 
+           return subtract(number1, number2);
+        break;
+        case "*": 
+            return multiply(number1, number2);
+        break;
+        case "/": 
+           return divide(number1, number2);
+        break;
+    }
+}
+
+function getOperator(word) {
+    let isOperator;
+    for(let i=0; i < word.length; i++) {
+        if( word.charAt(i) == "+" || word.charAt(i) == "-" || word.charAt(i) == "*" || word.charAt(i) == "/") {
+            isOperator = word.charAt(i);
+        }
+    }
+    
+    return isOperator;
+}
+
+
+function enterOperation() {
+    if(expression == "") console.log("is empty");
+    let operator = getOperator(expression);
+    let firstNumber = Number(expression.split(operator)[0]);
+    let secondNumber = Number(expression.split(operator)[1]);
+
+    result.textContent = operate(operator, firstNumber, secondNumber);
+}
+
+let expression = "";
+result.innerHTML = "";
+numberButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let number = button.textContent;
+        
+        expression += number;
+        result.textContent = expression;
+    })
+})
+
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let operator = button.textContent;
+
+        expression += operator;
+        result.textContent = expression;
+    })
+})
+
+enterButton.addEventListener("click", () => {
+    enterOperation();
+    expression = "";
+
+})
+/* const buttonsArray = [...buttons];
+const numberButtons = buttonsArray.filter((button) => Number(button.value)); */
+
+
+//let buttonNumbers = buttons.filter();
+//console.log(numberButtons);
+
